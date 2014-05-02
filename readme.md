91d69b9db1aa9e82216c3a77af2221004d499248
Mark Jaquith <mark@jaquith.me>
2014-05-02 00:04:24 -0400

    Initial version

---
+++ readme.md
@@ -0,0 +1,40 @@
+# Scheduled Post Guardian #
+
+Contributors: markjaquith  
+Donate link: http://txfx.net/wordpress-plugins/donate  
+Tags: future posts, scheduled posts, cron, nui  
+Requires at least: 3.8  
+Tested up to: 3.9  
+Stable tag: 1.0  
+
+Watches over scheduled posts, and makes sure they don't miss their scheduled time
+
+## Description ##
+
+Some WordPress sites have problems with scheduled posts, where they don't get published at the right time. Instead they appear as "missed schedule". This plugin monitors scheduled posts, and makes sure that doesn't happen.
+
+## Installation ##
+
+1. Upload the `scheduled-post-guardian` folder to your `/wp-content/plugins/` directory
+2. Activate the "Scheduled Post Guardian" plugin in your WordPress administration interface
+3. You're done! The plugin has no UI and it'll start working right away.
+
+## Frequently Asked Questions ##
+
+### How does it work? ###
+
+On "shutdown" (after WP finishes a request), it checks a timer. If it has been 3 minutes since it last checked, it looks at all the scheduled posts, and runs them through a WordPress core function that either publishes them (if they should have gone out already), or re-schedules them, if their schedule (their "cron" entry) has somehow gone away.
+
+### My post missed its schedule anyway! ###
+
+Normally, the plugin checks every 3 minutes. So it's possible the post might go out a few minutes late. I hope this isn't the end of the world. It's a tradeoff between this and checking all the time. Additionally, if a check determines that there are currently no scheduled posts, it won't look again for 15 minutes. Most people schedule posts more than 15 minutes in the future.
+
+### Can I change those times? ###
+
+Yes. There are filters: `scheduled_post_guardian_delay_minutes` and `scheduled_post_guardian_stretch_delay_minutes`. Return a different number than 3 and 15 for those if you like.
+
+## Changelog ##
+
+### 1.0 ###
+
+* Initial version, commissioned by OffbeatBride.com
---
+++ scheduled-post-guardian.php
@@ -0,0 +1,96 @@
+<?php
+/*
+Plugin Name: Scheduled Post Guardian
+Description: Watches over scheduled posts, so that no funny business interferes with their mission
+Version: 1.0
+License: GPLv2+
+Plugin URI: http://github.com/markjaquith/scheduled-post-guardian
+Author: Mark Jaquith
+Author URI: http://coveredwebservices.com/
+
+==========================================================================
+
+Copyright 2014  Mark Jaquith
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+// Bail on direct access
+defined('WPINC') or die;
+
+class Scheduled_Post_Guardian_Plugin extends WP_Stack_Plugin {
+	public static $instance;
+	const OPTION = 'scheduled_post_guardian_next_run';
+	const DELAY = 3; // Default is to check the future posts once every 3 minutes
+	const STRETCH = 15; // But this increases to 15 minutes if there were no scheduled posts waiting
+
+	public function __construct() {
+		self::$instance = $this;
+		// Yeah, this uses a shutdown hook, and not WP Cron
+		// Why? Well, some people having future post issues are having them because
+		// they have issues with cron firing reliably. So I won't want my fix to
+		// get made ineffective by the same issue.
+		add_action( 'shutdown', array( $this, 'shutdown' ) );
+		add_filter( 'scheduled_post_guardian_run', array( $this, 'run_on_edit_dot_php' ) );
+	}
+
+	public function shutdown() {
+		global $wpdb;
+
+		$next_run = get_option( self::OPTION, false );
+		$delay = apply_filters( 'scheduled_post_guardian_delay_minutes', self::DELAY );
+
+		// Should we run the check?
+		$run = $next_run === false || $next_run < time();
+
+		// Note, we hook in here to force-run for edit.php wp-admin requests
+		$run = apply_filters( 'scheduled_post_guardian_run', $run );
+
+		if ( ! $run ) {
+			return;
+		}
+
+		// Immediately bump the next_run forward (do this before any other queries to minimize race conditions)
+		update_option( self::OPTION, time() + $delay * 60 );
+
+		// You could use this filter to limit the query to a specific post type, I suppose
+		$where = apply_filters( 'scheduled_post_guardian_future_post_query_where', " `post_status`='future' " );
+
+		// Grab the scheduled posts from the database
+		$scheduled_posts = $wpdb->get_col( "SELECT `ID` FROM `{$wpdb->posts}` WHERE $where" );
+
+		if ( count( $scheduled_posts ) ) {
+			// If there are scheduled posts, run each through
+			// `check_and_publish_future_post()` which will either
+			// publish them or fix their schedule
+			foreach ( $scheduled_posts as $post_id ) {
+				check_and_publish_future_post( $post_id );
+			}
+		} else {
+			// If there are no posts waiting in the wings, we can probably delay the next check a bit
+			$stretch = apply_filters( 'scheduled_post_guardian_stretch_delay_minutes', self::STRETCH );
+			update_option( self::OPTION, time() + $stretch * 60 );
+		}
+	}
+
+	public function run_on_edit_dot_php( $run ) {
+		if ( ! $run && is_admin() && 'edit' === get_current_screen()->base ) {
+			return true;
+		}
+		return $run;
+	}
+}
+
+new Scheduled_Post_Guardian_Plugin;
